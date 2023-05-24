a270e3205ed10fe27ad4eafa4dbc95c595584011
José Ruiz <ruiz211@duck.com>
2023-05-24 11:33:20 -0500

    Project initialisation

---
+++ Ultimate Portfolio/Ultimate_PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ultimate_PortfolioApp.swift
+//  Ultimate Portfolio
+//
+//  Created by José Ruiz on 24/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Ultimate_PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
